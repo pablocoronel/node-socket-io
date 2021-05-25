9176bc651fe46d3fb55a701bd1d1e848c557c4f0
Pablo Coronel <pablomanuelcoronel@gmail.com>
2021-05-25 16:16:04 -0300

    feat: config socket io

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path'); // Manejar paths independientemente del SO
 const express = require('express');
 const app = express();
+const socketIO = require('socket.io'); // Manejo de sockets
 
 // Settings
 app.set('port', process.env.PORT || 3000);
@@ -9,6 +10,11 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public'))); // Cargar los archivos estaticos en la app
 
 // Start the server
-app.listen(app.get('port'), () => {
+const server = app.listen(app.get('port'), () => {
 	console.log('Server on port', app.get('port'));
 });
+
+// Config de socket io
+const io = socketIO.listen(server); // Recibe un server, mantiene la conexion de sockets
+
+// Websockets
